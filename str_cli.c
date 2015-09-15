cd9128257198b8ef16fbc1b400e71c627b7ad053
Chaofan Li <li3939108@gmail.com>
2015-09-14 20:49:02 -0500

    update

---
+++ str_cli.c
@@ -0,0 +1,18 @@
+#include "commons.h"
+#include <readline/readline.h>
+#include <readline/history.h>
+#include <stdio.h>
+
+extern void Writen(int fd, void *ptr, size_t nbytes);
+extern void err_quit(const char *fmt, ...);
+
+void str_cli(FILE *fp, int sockfd){
+	char	sendline[MAXLINE], recvline[MAXLINE];
+	while (fgets(sendline, MAXLINE, fp) != NULL) {
+		Writen(sockfd, sendline, strlen(sendline));
+		if (readline(sockfd, recvline, MAXLINE) == 0){
+			err_quit("str_cli: server terminated prematurely");
+		}
+		fputs(recvline, stdout);
+	}
+}
